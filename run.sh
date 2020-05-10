dcce0b2032f7cde103562032aa2ff716a80278e7
Rootpaceusa <64696314+ahmadchenwangxuesi@users.noreply.github.com>
2020-05-10 23:53:23 +0700

    .

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg install wget python unrar figlet -y
+echo
+echo =====================================================
+figlet "Cloud-IP" 
+echo
+figlet "Download"
+echo
+echo =====================================================
+echo
+wget 'https://drive.google.com/uc?id=1QhdSCLmWvx4hdgfqm_4FRdgbnOoBSbP-&export=download' -O Cloud-IP.rar
+echo
+echo =====================================================
+figlet "Extrak File"
+echo
+unrar x Cloud-IP.rar CloudIP
+echo
+echo =====================================================
+figlet "Hapus File"
+echo 
+rm Cloud-IP.rar
+echo
+figlet "Ready Tools"
+echo =====================================================
+echo
+echo
+echo
+figlet "Regard"
+echo =====================================================
+figlet "PaceUSA"
+echo =====================================================
+figlet "Developer"
+echo =====================================================
+figlet "root"
+echo =====================================================
+echo
